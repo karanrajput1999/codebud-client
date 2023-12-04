7c787ba90d6a0215c761f5a885d5421e856cf890
Karan <rajputkaran441@gmail.com>
2023-12-04 09:03:48 +0530

    routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
-// import LandingPage from "./components/landingpage/LandingPage";
-// import Signup from "./components/signup/Signup";
-import Login from "./components/Login.tsx/Login";
+import LandingPage from "./components/landingpage/LandingPage";
+import Signup from "./components/signup/Signup";
+import Login from "./components/login/Login";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import Root from "./Root";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="" element={<LandingPage />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      {/* <LandingPage /> */}
-      {/* <Signup /> */}
-      <Login />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import { useState } from "react";
 import { cn } from "@/lib/utils";
 
@@ -15,13 +17,14 @@ function Navbar() {
   return (
     <header>
       <nav className="h-[4rem] flex items-center justify-around border-b border-primarycb px-2">
-        <div className="logo flex md:gap-5 gap-3 items-center mr-2 overflow-hidden px-1 ">
-          <img src={logo} alt="logo" />
-          <span className="text-[2rem] min-[450px]:block hidden font-bold ">
-            Codebud
-          </span>
-        </div>
-
+        <Link to="/">
+          <div className="logo flex md:gap-5 gap-3 items-center mr-2 overflow-hidden px-1 ">
+            <img src={logo} alt="logo" />
+            <span className="text-[2rem] min-[450px]:block hidden font-bold ">
+              Codebud
+            </span>
+          </div>
+        </Link>
         <div className="searchbar sm:w-2/5 w-3/5 mx-2 sm:block max-[450px]:w-3/4 ">
           <Input
             className="border border-primarycb"
@@ -31,10 +34,10 @@ function Navbar() {
 
         <div className="login-container sm:flex md:gap-5 sm:gap-2 gap-1 hidden">
           <Button className="md:text-lg text-base md:w-24 sm:w-20 w-15 bg-primarycb border hover:bg-white hover:text-primarycb hover:border-primarycb">
-            Login
+            <Link to="/login">Login</Link>
           </Button>
           <Button className="md:text-lg text-base md:w-24 w-20 bg-primarycb border hover:bg-white hover:text-primarycb hover:border-primarycb">
-            Signup
+            <Link to="/signup">Signup</Link>
           </Button>
         </div>
 
@@ -49,15 +52,15 @@ function Navbar() {
               mobileLogInMenuOpen ? "flex" : "hidden"
             )}
           >
-            <a
-              href="#"
+            <Link
+              to="/login"
               className="text-center font-semibold py-1 border border-b-primarycb"
             >
               Login
-            </a>
-            <a href="#" className="text-center font-semibold py-1">
+            </Link>
+            <Link to="/signup" className="text-center font-semibold py-1">
               Signup
-            </a>
+            </Link>
           </div>
         </div>
       </nav>
