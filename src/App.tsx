ffba9ee355f1ae493085bfe3204b109f78cdf4fe
Karan <rajputkaran441@gmail.com>
2023-12-04 09:05:53 +0530

    deployment error resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import Navbar from "./components/navbar/Navbar";
 import LandingPage from "./components/landingpage/LandingPage";
 import Signup from "./components/signup/Signup";
 import Login from "./components/login/Login";
