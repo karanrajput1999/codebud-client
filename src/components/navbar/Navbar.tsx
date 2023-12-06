0c7e71060cef0cd38ede8a37e637ca0324e16510
Karan <rajputkaran441@gmail.com>
2023-12-06 21:53:10 +0530

    replaced normal form with react-hook-form in signup

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -50,12 +50,16 @@ function Navbar() {
         </div>
 
         <div className="login-container sm:flex md:gap-5 sm:gap-2 gap-1 hidden">
-          <Button className="md:text-lg text-base md:w-24 sm:w-20 w-15 bg-primarycb border hover:bg-white hover:text-primarycb hover:border-primarycb">
-            <Link to="/login">Login</Link>
-          </Button>
-          <Button className="md:text-lg text-base md:w-24 w-20 bg-primarycb border hover:bg-white hover:text-primarycb hover:border-primarycb">
-            <Link to="/signup">Signup</Link>
-          </Button>
+          <Link to="/login">
+            <Button className="md:text-lg text-base md:w-24 sm:w-20 w-15 bg-primarycb border hover:bg-white hover:text-primarycb hover:border-primarycb">
+              Login
+            </Button>
+          </Link>
+          <Link to="/signup">
+            <Button className="md:text-lg text-base md:w-24 w-20 bg-primarycb border hover:bg-white hover:text-primarycb hover:border-primarycb">
+              Signup
+            </Button>
+          </Link>
         </div>
 
         <div className="mobile-login-menu-container relative sm:hidden flex items-center">
--- src/components/signup/Signup.tsx
+++ src/components/signup/Signup.tsx
@@ -2,7 +2,49 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z.string().email(),
+  password: z.string().min(4, {
+    message: "Password must be atleast 4 chars long.",
+  }),
+  confirmPassword: z.string().min(4, {
+    message: "Password must be atleast 4 chars long.",
+  }),
+});
+
 function Signup() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <main
       className="flex items-center justify-center px-5 pt-5 "
@@ -13,38 +55,86 @@ function Signup() {
           Signup
         </span>
 
-        <form>
-          <div className="flex flex-col gap-1">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            {/* <div className="flex flex-col gap-1">
             <label htmlFor="name">Name</label>
             <Input type="text" id="name" className="mb-2" />
-          </div>
-          <div className="flex flex-col gap-1">
-            <label htmlFor="email">Email</label>
-            <Input type="email" id="email" className="mb-2" />
-          </div>
-          <div className="flex flex-col gap-1">
-            <label htmlFor="password">Password</label>
-            <Input type="password" id="password" className="mb-2" />
-          </div>
-          <div className="flex flex-col gap-1">
-            <label htmlFor="confirmPassword">Confirm password</label>
-            <Input type="password" id="confirmPassword" className="mb-2" />
-          </div>
-          <div className="flex justify-center mt-3">
-            <Button className="bg-transparent h-8 text-primarycb border border-primarycb hover:bg-white">
-              Choose Avatar
-            </Button>
-          </div>
+          </div> */}
 
-          <div className="flex justify-center mt-5">
-            <Button
-              type="submit"
-              className="bg-primarycb text-lg w-28 hover:bg-primarycb "
-            >
-              Signup
-            </Button>
-          </div>
-        </form>
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="John doe" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="someone@example.com" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Your password" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Confirm your password" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex justify-center mt-3">
+              <Button className="bg-transparent h-8 text-primarycb border border-primarycb hover:bg-white">
+                Choose Avatar
+              </Button>
+            </div>
+
+            <div className="flex justify-center mt-5">
+              <Button
+                type="submit"
+                className="bg-primarycb text-lg w-28 hover:bg-primarycb "
+              >
+                Signup
+              </Button>
+            </div>
+          </form>
+        </Form>
         <span className="mt-5 block text-center">
           Already registered ?{" "}
           <Link to="/login" className="underline text-primarycb">
