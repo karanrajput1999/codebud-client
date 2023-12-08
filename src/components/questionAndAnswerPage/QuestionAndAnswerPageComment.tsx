f79b28de4b663df08b465385bd5cbdb34d6e8e33
Karan <rajputkaran441@gmail.com>
2023-12-08 18:54:09 +0530

    comment form toggeling

--- src/components/questionAndAnswerPage/QuestionAndAnswerPageComment.tsx
+++ src/components/questionAndAnswerPage/QuestionAndAnswerPageComment.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 
 function QuestionAndAnswerPageComment() {
+  const [toggleCommentForm, setToggleCommentForm] = useState(false);
+
+  function showCommentForm() {
+    setToggleCommentForm(!toggleCommentForm);
+  }
+
+  function cancelForm(e) {
+    e.preventDefault();
+    setToggleCommentForm(false);
+  }
+
   return (
     <div>
       <div className="comment border-b border-slate-200">
@@ -19,13 +31,37 @@ function QuestionAndAnswerPageComment() {
           <button className="text-primarycb">Delete</button>
         </div>
       </div>{" "}
-      <div className="add-comment text-sm text-slate-500 py-2 border-b border-slate-200">
-        <button>Add comment</button>
+      <div
+        className={`add-comment text-sm text-slate-500 py-2 border-b border-slate-200 ${
+          toggleCommentForm ? "hidden" : "block"
+        }`}
+      >
+        <button onClick={showCommentForm}>Add comment</button>
       </div>
-      <div className="comment-form hidden">
+      <div
+        className={`comment-form mt-2 ${
+          toggleCommentForm ? "block" : "hidden"
+        }`}
+      >
         <form>
-          <Textarea />
-          <Button>Comment</Button>
+          <div className="flex gap-5">
+            <Textarea />
+
+            <div className="flex flex-col gap-2 pr-2">
+              <Button
+                type="submit"
+                className="text-white bg-primarycb hover:bg-primarycb hover:text-white"
+              >
+                Comment
+              </Button>
+              <Button
+                className="bg-white border border-red-600 text-red-600 hover:text-white hover:bg-red-600"
+                onClick={cancelForm}
+              >
+                Cancel
+              </Button>
+            </div>
+          </div>
         </form>
       </div>
     </div>
